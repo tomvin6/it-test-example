a93d13fad00fdcb8c1b323af84902c1b0138f61f
vaingart <tomvin>
2016-03-06 11:05:01 +0200

    new files

--- app-module/src/test/java/com/soebes/maui/it/TestUnitExample.java
+++ app-module/src/test/java/com/soebes/maui/it/TestUnitExample.java
@@ -1,21 +1,26 @@
 package com.soebes.maui.it;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 /**
  * Created by vaingart on 06/03/2016.
  */
-public class TestUnitExample extends TestCase {
+public class TestUnitExample {
 
     @Test
-    public void checkFirstBitTest() {
+    public void successTestC() {
         BitMask bm = new BitMask(0x8000000000000000L);
         assertEquals(true, true);
     }
 
     @Test
-    public void checkNumberBitTest() {
+    public void successTestB() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void successTestA() {
         assertEquals(true, true);
     }
 }
